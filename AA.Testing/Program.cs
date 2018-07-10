7ba3a51733fc66dc8cd318dedcfa566dff9010ca
ChengLab <kaixincheng2007@163.com>
2018-07-10 10:01:03 +0800

    log Usage

--- AA.Testing/Program.cs
+++ AA.Testing/Program.cs
@@ -10,6 +10,19 @@ namespace AA.Testing
     {
         static void Main(string[] args)
         {
+
+            //NET croe Usage  step ->log4net 
+            //1
+            //string path = AppDomain.CurrentDomain.BaseDirectory;
+            //var file = Path.Combine(path, "LogCons.log4net.config");
+            //var logRepository = LogManager.GetRepository(System.Reflection.Assembly.GetEntryAssembly());
+            //XmlConfigurator.ConfigureAndWatch(logRepository, new FileInfo(file));
+            //Log4NetLogger.Use();
+
+            //2
+            // ILog log = Logger.Get(typeof(ValuesController));
+            // log.Debug("AA.Framework debug");
+
         }
     }
 }
